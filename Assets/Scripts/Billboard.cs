87c674f1523a844937fd18255f771356dbee0ebb
Karim <102354536+Karim-Nessim@users.noreply.github.com>
2026-04-07 13:36:17 -0700

    menu

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -143,7 +143,7 @@ public class Billboard : MonoBehaviour
             transform.rotation = targetRotation;
         }
 
-        // 🔽 NEW: Cache this rotation as the new resting rotation
+        //Cache this rotation as the new resting rotation
         lastBillboardRotation = transform.rotation;
 
         wasInRange = isInRange;
